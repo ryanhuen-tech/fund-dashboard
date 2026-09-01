7df7e1c28d55a412ead76622e0149c7707f7bf1f
ryanhuen-tech <ryanhuen@gmail.com>
2026-09-01 21:11:28 +0800

    Update __init__.py

--- funds/__init__.py
+++ funds/__init__.py
@@ -20,8 +20,7 @@ from funds.fund_z51 import DATA_Z51
 from funds.fund_z52 import DATA_Z52
 from funds.fund_z69 import DATA_Z69
 from funds.fund_z77 import DATA_Z77
-from funds.fund_zP4 import DATA_ZU6
-from funds.fund_zP4 import DATA_ZP4
+from funds.fund_zp4 import DATA_ZP4
 
 ALL_FUNDS = {
     **DATA_Z01,
@@ -31,6 +30,7 @@ ALL_FUNDS = {
     **DATA_Z06,
     **DATA_Z07,
     **DATA_Z08,
+    **DATA_Z12,
     **DATA_Z13,
     **DATA_Z15,
     **DATA_Z17,
@@ -43,6 +43,5 @@ ALL_FUNDS = {
     **DATA_Z52,
     **DATA_Z69,
     **DATA_Z77,
-    **DATA_ZU6,
     **DATA_ZP4,
 }
