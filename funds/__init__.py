b9226a472d8f91d03c4e947f5f8e4d5762c1152f
ryanhuen-tech <ryanhuen@gmail.com>
2026-09-01 19:22:25 +0800

    Update __init__.py

--- funds/__init__.py
+++ funds/__init__.py
@@ -1,1 +1,17 @@
+# funds/__init__.py
 
+from funds.fund_z03 import DATA_Z03
+from funds.fund_z07 import DATA_Z07
+from funds.fund_z18 import DATA_Z18
+from funds.fund_z20 import DATA_Z20
+from funds.fund_z33 import DATA_Z33
+from funds.fund_z77 import DATA_Z77
+
+ALL_FUNDS = {
+    **DATA_Z03,
+    **DATA_Z07,
+    **DATA_Z18,
+    **DATA_Z20,
+    **DATA_Z33,
+    **DATA_Z77,
+}
